d04923185b061403260c1217385d108de457d8f3
mohsin-islam2024 <152082206+mohsin-islam2024@users.noreply.github.com>
2023-12-13 19:17:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "1da8204e90f767de2187656f582c6d91";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather (city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
