038a5f9a265ae1e8f660fe92e6f861cedc95171b
mohsin-islam2024 <152082206+mohsin-islam2024@users.noreply.github.com>
2023-12-16 14:40:22 +0600

    add weather icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 
 async function checkWeather (city) {
@@ -10,10 +11,39 @@ async function checkWeather (city) {
     var data = await response.json();
     console.log(data);
 
-    document.querySelector(".city").innerHTML = data.name;
+    if (data.name == undefined) {
+        document.querySelector(".city").innerHTML = "City name not found";
+        
+    } else {
+
+        document.querySelector(".city").innerHTML = data.name;
+    }
+    
+    
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+
+    } 
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "images/rain.png";
+
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "images/drizzle.png";
+
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "images/mist.png";
+
+    }
 
 }
 
